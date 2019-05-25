c6a2adf3bbd028251d0478af17f753cdc1e87a4e
Abhiraj Pundir <pundirabhiraj.com>
2019-05-25 10:03:14 +0530

    Creating UrlConf

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generics import ListView
+from django.views.generic import ListView
 from core.models import Movie
 
 class MovieList(ListView):
