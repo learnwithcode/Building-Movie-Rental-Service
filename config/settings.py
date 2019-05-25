ffd2f171bafbe8ad479fc44fc35bbc580f34b071
Abhiraj Pundir <pundirabhiraj.com>
2019-05-25 10:46:39 +0530

    Adding 404 page

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(((m7oywt*p@f=jd(u81ho6&c2trzmt*sjkh^0=!m-m7jr^m%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
